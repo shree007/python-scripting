8e245d73eac0bad120d18966dad6bb657f4ac750
Shree Prakash Gupta <shreeprakashagrahari05@gmail.com>
2020-02-16 13:06:25 +0530

    adding, modifying env var

--- get-specific-env-variable.py
+++ get-specific-env-variable.py
@@ -4,7 +4,18 @@ import pprint
 
 
 def driver():
+	# get env variable
 	pprint.pprint(os.environ.get('JAVA_HOME'))
+
+	# adding new env variable
+	os.environ['zero'] = 'shree'
+	pprint.pprint(os.environ['zero'])
+
+	# modify env variable
+
+	os.environ['zero']='prakash'
+	pprint.pprint(os.environ['zero'])
+
 
 
 if __name__ == "__main__":
