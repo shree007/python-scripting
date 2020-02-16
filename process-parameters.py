a58386bb5d89cd1557a57b4229064eb4962c5b9a
Shree Prakash Gupta <shreeprakashagrahari05@gmail.com>
2020-02-16 22:14:21 +0530

    adding process parameters

---
+++ process-parameters.py
@@ -0,0 +1,29 @@
+import os
+import pprint
+
+
+def driver():
+	# process parameters
+	pprint.pprint(os.ctermid()) # return corressponding file name
+
+	# return effective group id of cureent process
+	pprint.pprint(os.getegid())
+
+	# get effective user id of current process
+	pprint.pprint(os.geteuid())
+
+	# get real group id of current process
+	pprint.pprint(os.getgid())
+
+	# get real user id of current process
+	pprint.pprint(os.getuid())
+
+	# get logged in username
+	pprint.pprint(os.system('whoami'))
+
+	# get pid of group
+	pprint.pprint(os.getpgid(os.getpid()))
+
+
+if __name__ == "__main__":
+	driver()
