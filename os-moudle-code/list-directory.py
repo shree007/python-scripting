09de5766c731b6e7de9c225a1755f0f73769f204
shree007 <shreeprakashagrahari05@gmail.com>
2024-10-16 00:45:15 +0530

    improve list directory codes

--- os-moudle-code/list-directory.py
+++ os-moudle-code/list-directory.py
@@ -1,10 +1,18 @@
 import os
 
+def get_files(directory_name):
+    return os.listdir(directory_name)
 
-def driver():
-	path="."
-	print(os.listdir(path))
-
-
-if __name__ == "__main__":
-	driver()
+if __name__ == '__main__':
+    try:
+        directory_name = input("Enter directory name: ").strip()
+        if len(directory_name) != 0:
+            list_of_files = get_files(directory_name)
+            if len(list_of_files) != 0:
+                print(list_of_files)
+            else:
+                print("Directory is empty")
+        else:
+            print("Empty Directory")
+    except Exception as e:
+        print("Invalid Directory", e)
