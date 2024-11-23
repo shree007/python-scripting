783b313931229ecd80d782f29b55f590df9ddbe2
shree007 <shreeprakashagrahari05@gmail.com>
2024-11-23 23:38:46 +0530

    Nov-23 implement inmemory get using dictionary

--- inmemory-operations/inmemory-ops-implementation.py
+++ inmemory-operations/inmemory-ops-implementation.py
@@ -5,8 +5,14 @@ class inMemory:
     def add(self, key, value):
         self.store[key]=value
         return f"key '{key}' has been added"
+    
+    def get(self, key):
+        if key in self.store:
+            return self.store[key]
+        return f"key '{key}' is not found"
 
 
 if __name__ =='__main__':
     db = inMemory()
     print(db.add("hello",9))
+    print(db.get("hello"))
